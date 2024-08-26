49f95ee8513c174167bf0cf96469901501a3c34c
Abdullah Alhwyji <abdullah.alhwyji@gmail.com>
2024-08-26 11:35:32 +0700

    2nd commit

--- main.go
+++ main.go
@@ -5,10 +5,17 @@ import (
 	"net/http"
 
 	"github.com/abdullahalhwyji/crud_web_golang/config"
+	"github.com/abdullahalhwyji/crud_web_golang/controllers/homecontroller"
 )
 
 func main() {
 	config.ConnectDB()
+
+	// 1. homepage
+	http.HandleFunc("/", homecontroller.Welcome)
+
+	//2. catagories
+
 	log.Println("Server is runing on port 8000 ...")
 
 	http.ListenAndServe(":8000", nil)
