750c3ec9b06334faf6081dbb60dd02004a0314bb
Abdullah Alhwyji <abdullah.alhwyji@gmail.com>
2024-08-26 17:27:03 +0700

    edit function

--- controllers/catagoriescontroller/catagoriescontroller.go
+++ controllers/catagoriescontroller/catagoriescontroller.go
@@ -3,6 +3,7 @@ package catagoriescontroller
 import (
 	"html/template"
 	"net/http"
+	"strconv"
 	"time"
 
 	"github.com/abdullahalhwyji/crud_web_golang/entities"
@@ -51,6 +52,23 @@ func Add(w http.ResponseWriter, r *http.Request) {
 	}
 }
 func Edit(w http.ResponseWriter, r *http.Request) {
+	if r.Method == "GET" {
+		temp , err := template.ParseFiles("views/category/edit.html")
+		if err != nil {
+			panic(err)
+		}
+
+		idString := r.URL.Query().Get("id")
+		id, err := strconv.Atoi(idString)
+		if err != nil{
+			panic(err)
+		}
+		category := categorymodel.Detail(id)
+		data := map[string]any {
+			"category": category,
+		}
+		temp.Execute(w, data)
+	}
 
 }
 func Delete(w http.ResponseWriter, r *http.Request) {
